40e6e0cc383628d9d23b925bd093dd727b9eaa57
amram <eliran0543@gmail.com>
2020-05-02 16:36:53 +0300

    Updae Semaphore

--- Semaphore/Semaphore.cpp
+++ Semaphore/Semaphore.cpp
@@ -3,23 +3,23 @@
 
 
 
-Semaphore::Semaphore() throw (runtime_error, bad_alloc)
+Semaphore::Semaphore()
 {
-	sem_init(m_semaphore, 0, 0);
+	sem_init(&m_semaphore, 0, 0);
 }
 
 Semaphore::~Semaphore()
 {
-	sem_destroy(m_semaphore);
+	sem_destroy(&m_semaphore);
 }
 
 void Semaphore::Post()
 {
-	sem_post(m_semaphore);
+	sem_post(&m_semaphore);
 }
 
 void Semaphore::Wait()
 {
-	sem_wait(m_semaphore);
+	sem_wait(&m_semaphore);
 }
 
